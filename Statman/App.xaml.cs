5bd97b23a16895caa8a56e6d9763b3ae9d7a6f7e
Orfeas Zafeiris <orfeaz@gmail.com>
2015-08-17 21:41:23 +0300

    Refactoring the H3 stat modules.
    Now only allowing a single instance of Statman to run at any given point in time.

--- Statman/App.xaml.cs
+++ Statman/App.xaml.cs
@@ -1,17 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using Statman.Util;
 
 namespace Statman
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : Application, ISingleInstanceApp
     {
+        [STAThread]
+        public static void Main()
+        {
+            // Only allow a single instance of Statman
+            // to run at any given point in time.
+            if (!SingleInstance<App>.InitializeAsFirstInstance("Statman_4e29fa")) 
+                return;
+
+            var s_Application = new App();
+
+            s_Application.InitializeComponent();
+            s_Application.Run();
+
+            SingleInstance<App>.Cleanup();
+        }
+
+        public bool SignalExternalCommandLineArgs(IList<string> p_Args)
+        {
+            if (MainApp.MainWindow != null)
+                MainApp.MainWindow.Activate();
+
+            return true;
+        }
     }
 }
