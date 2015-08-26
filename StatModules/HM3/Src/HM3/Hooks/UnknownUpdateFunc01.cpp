00c012500aac436582c97c714e15c2548e45797a
Orfeas Zafeiris <orfeaz@gmail.com>
2015-08-26 20:01:56 +0300

    Hotfix for crash when storing custom weapons (fixes #1).
    Bumped version to 1.3.1.0.

--- StatModules/HM3/Src/HM3/Hooks/UnknownUpdateFunc01.cpp
+++ StatModules/HM3/Src/HM3/Hooks/UnknownUpdateFunc01.cpp
@@ -41,13 +41,13 @@ char __fastcall HM3Hooks::c_UnknownUpdateFunc01(UnknownClass01* th, int)
 
 	UnknownClass03* s_Class03 = *(UnknownClass03**) 0x0081F83C;
 
-	if (s_Class03 && th->m_Unknown03)
+	if (s_Class03 && th->m_Unknown03 && s_Class03->m_Unknown02)
 	{
 		g_Module->Functions()->UnknownFunction01(th->m_Unknown03, s_Class03->m_Unknown02);
 		s_WeaponsInHand += g_Module->Functions()->UnknownFunction02(th->m_Unknown03);
 	}
 
-	if (s_Class03 && th->m_Unknown04)
+	if (s_Class03 && th->m_Unknown04 && s_Class03->m_Unknown02)
 	{
 		g_Module->Functions()->UnknownFunction01(th->m_Unknown04, s_Class03->m_Unknown02);
 		s_WeaponsInHand += g_Module->Functions()->UnknownFunction02(th->m_Unknown04);
