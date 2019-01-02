9dc9897206ea2876f7b8082f5ac0428ecbab9f29
Orfeas Zafeiris <orfeaz@gmail.com>
2019-01-02 02:53:09 +0200

    [HM5] Update the engine control to only contain relevant data

--- Statman/Engines/HM5/Controls/MainControl.xaml.cs
+++ Statman/Engines/HM5/Controls/MainControl.xaml.cs
@@ -19,133 +19,33 @@ namespace Statman.Engines.HM5.Controls
             });
         }
         
-        public void SetCurrentTime(double p_Time)
+        public void SetGameTime(double p_Time)
         {
-            SetCurrentTime((long)(p_Time * 1000.0));
+            SetGameTime((long)(p_Time * 1000.0));
         }
 
-        public void SetCurrentTime(long p_Time)
+        public void SetGameTime(long p_Time)
         {
             Dispatcher.Invoke(() =>
             {
                 var s_TimeStamp = TimeSpan.FromMilliseconds(p_Time);
-                CurrentTimeLabel.Content = s_TimeStamp.ToString("hh\\:mm\\:ss\\.fff");
+                GameTimeLabel.Content = s_TimeStamp.ToString("hh\\:mm\\:ss\\.fff");
             });
         }
 
-        public void SetRating(string p_Rating)
+        public void SetRealTime(double p_Time)
+        {
+            SetRealTime((long)(p_Time * 1000.0));
+        }
+
+        public void SetRealTime(long p_Time)
         {
             Dispatcher.Invoke(() =>
             {
-                RatingLabel.Content = p_Rating.Replace("_", "__");
+                var s_TimeStamp = TimeSpan.FromMilliseconds(p_Time);
+                RealTimeLabel.Content = s_TimeStamp.ToString("hh\\:mm\\:ss\\.fff");
             });
         }
 
-        public void SetRatingPerfect(bool p_Perfect)
-        {
-            Dispatcher.Invoke(() =>
-            {
-                if (p_Perfect)
-                    RatingLabel.Foreground = (Brush) FindResource("LabelBrush");
-                else
-                    RatingLabel.Foreground = (Brush) FindResource("AlertLabelBrush");
-            });
-        }
-        
-        public void SetDisguiseBroken(bool p_Value)
-        {
-            Dispatcher.Invoke(() =>
-            {
-                DisguiseBrokenLabel.Content = p_Value ? "Yes" : "No";
-            });
-        }
-        
-        public void SetDisguiseSuspicious(bool p_Value)
-        {
-            Dispatcher.Invoke(() =>
-            {
-                DisguiseSuspiciousLabel.Content = p_Value ? "Yes" : "No";
-            });
-        }
-        
-        public void SetTrespassing(bool p_Value)
-        {
-            Dispatcher.Invoke(() =>
-            {
-                TrespassingLabel.Content = p_Value ? "Yes" : "No";
-            });
-        }
-        
-        public void SetDeepTrespassing(bool p_Value)
-        {
-            Dispatcher.Invoke(() =>
-            {
-                DeepTrespassingLabel.Content = p_Value ? "Yes" : "No";
-            });
-        }
-        
-        public void SetSpottedTrespassing(bool p_Value)
-        {
-            Dispatcher.Invoke(() =>
-            {
-                SpottedTrespassingLabel.Content = p_Value ? "Yes" : "No";
-            });
-        }
-        
-        public void SetBodyFoundPacify(bool p_Value)
-        {
-            Dispatcher.Invoke(() =>
-            {
-                BodyFoundPacifyLabel.Content = p_Value ? "Yes" : "No";
-            });
-        }
-        
-        public void SetBodyFoundMurder(bool p_Value)
-        {
-            Dispatcher.Invoke(() =>
-            {
-                BodyFoundMurderLabel.Content = p_Value ? "Yes" : "No";
-            });
-        }
-        
-        public void SetBodyFoundAccident(bool p_Value)
-        {
-            Dispatcher.Invoke(() =>
-            {
-                BodyFoundAccidentLabel.Content = p_Value ? "Yes" : "No";
-            });
-        }
-        
-        public void SetBloodPoolFound(bool p_Value)
-        {
-            Dispatcher.Invoke(() =>
-            {
-                BloodPoolFoundLabel.Content = p_Value ? "Yes" : "No";
-            });
-        }
-        
-        public void SetSpottedEnteringCloset(bool p_Value)
-        {
-            Dispatcher.Invoke(() =>
-            {
-                SpottedEnteringClosetLabel.Content = p_Value ? "Yes" : "No";
-            });
-        }
-        
-        public void SetDeadBodySeenMurder(bool p_Value)
-        {
-            Dispatcher.Invoke(() =>
-            {
-                DeadBodySeenMurderLabel.Content = p_Value ? "Yes" : "No";
-            });
-        }
-        
-        public void SetDeadBodySeenAccident(bool p_Value)
-        {
-            Dispatcher.Invoke(() =>
-            {
-                DeadBodySeenAccidentLabel.Content = p_Value ? "Yes" : "No";
-            });
-        }
     }
 }
