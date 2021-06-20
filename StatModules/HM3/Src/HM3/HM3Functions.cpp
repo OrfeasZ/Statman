f59cf6aa4a628a79a265cabb40f58e85978b6a46
aarmono <33583235+aarmono@users.noreply.github.com>
2021-06-19 18:21:25 -0700

    Update most HM3Functions addresses

--- StatModules/HM3/Src/HM3/HM3Functions.cpp
+++ StatModules/HM3/Src/HM3/HM3Functions.cpp
@@ -12,9 +12,9 @@ HM3Functions::~HM3Functions()
 
 void HM3Functions::Setup()
 {
-	GetNPCByID = (GetNPCByID_t) 0x004E5110;
-	SelectedGUIElement = (SelectedGUIElement_t) 0x0055FA10;
-	UnknownFunction01 = (UnknownFunction01_t) 0x00656A10;
-	UnknownFunction02 = (UnknownFunction02_t) 0x00656A60;
-	GetNPCWeaponCount = (GetNPCWeaponCount_t) 0x006AE000;
+	GetNPCByID = (GetNPCByID_t) 0x004E5BE0;
+	//SelectedGUIElement = (SelectedGUIElement_t) 0x0055FA10;
+	UnknownFunction01 = (UnknownFunction01_t) 0x00657170;
+	UnknownFunction02 = (UnknownFunction02_t) 0x006571C0;
+	GetNPCWeaponCount = (GetNPCWeaponCount_t) 0x006AE880;
 }
